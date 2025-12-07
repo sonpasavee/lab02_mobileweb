642dbf2a7af56412d241834f7e72929385677e91
sonpasavee975 <sunpasavee@gmail.com>
2025-12-07 14:43:40 +0700

    first commit

---
+++ apps/lab02_vue/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lab02_vue',
+  webDir: 'dist'
+};
+
+export default config;
